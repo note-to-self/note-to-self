87b712bf3902376da425dda59484c92f824b7f21
Marcy Silverman <marcysilve225@gmail.com>
2019-03-23 14:38:16 -0700

    added home component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import HomeModal from '../home/HomeModal';
-import homeImage from '../../../assets/images/home-image.jpg';
-import styles from 'styled-components';
-
-// const HomeImage = styles.body`
-// @media (min-width: 375px) {
-//   background-image: url(${homeImage}); 
-//   height: 100vh; 
-//   background-position: center;
-//   background-repeat: no-repeat;
-//   background-size: 100vw 100vh;
-// }
-// `;
+import Home from '../home/Home';
 
 
 
@@ -19,6 +8,7 @@ export default function App() {
   return (
     <>
     <HomeModal/>
+    <Home/>
     </>
   );
 }
