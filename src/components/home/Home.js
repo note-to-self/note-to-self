de3fec355bae0cd7cbcc99752eb6e24029a93252
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-25 17:29:14 -0700

    collect all the data needed to create a message in redux

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,21 +1,21 @@
 import React, { PureComponent } from 'react';
-import styles from 'styled-components';
-import homeImage from '../../../assets/images/home-image.jpg';
+// import styles from 'styled-components';
+// import homeImage from '../../../assets/images/home-image.jpg';
 import 'normalize.css';
 import PropTypes from 'prop-types';
 import {
   Link
 } from 'react-router-dom';
 
-const HomeImage = styles.body`
-@media (min-width: 375px) {
-  background-image: url(${homeImage}); 
-  height: 100vh; 
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 100vw 100vh;
-}
-`;
+// const HomeImage = styles.body`
+// @media (min-width: 375px) {
+//   background-image: url(${homeImage}); 
+//   height: 100vh; 
+//   background-position: center;
+//   background-repeat: no-repeat;
+//   background-size: 100vw 100vh;
+// }
+// `;
 export default class Home extends PureComponent {
   static propTypes = {
     logout: PropTypes.func,
