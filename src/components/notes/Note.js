2ccddb5a4d6a315dcd31d261834a3f37fea400ae
Kate Dameron <katerj33@gmail.com>
2019-03-27 09:52:29 -0700

    updated Note comp with func props and new snap

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Note({ body, schedule, checkboxLabel }) {
+function Note({ body, handleSubmit, schedule, checkboxLabel, checkboxValue, handleCheckbox }) {
   return (
     <section>
-      <form>
+      {handleSubmit && <form onSubmit={handleSubmit}>
         <label htmlFor={checkboxLabel}>{checkboxLabel}</label>
-        <input type="checkbox" name={checkboxLabel} value={checkboxLabel}/>
-      </form>
+        <input onChange={handleCheckbox} type="checkbox" name={checkboxLabel} value={checkboxValue}/>
+        <button>{checkboxLabel}</button>
+      </form>}
       <h3>{body}</h3>
-      <p>This is a {schedule} affirmation.</p>
+      {schedule && <p>This is a {schedule} affirmation.</p>}
     </section>
   );
 }
 
 Note.propTypes = {
   body: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func,
   schedule: PropTypes.string,
-  checkboxLabel: PropTypes.string
+  checkboxLabel: PropTypes.string,
+  checkboxValue: PropTypes.bool,
+  handleCheckbox: PropTypes.func
 };
 
 export default Note;
--- src/components/notes/Note.test.js
+++ src/components/notes/Note.test.js
@@ -5,11 +5,16 @@ import Note from './Note';
 
 describe('Note snapshot', () => {
   it('matches a snapshot', () => {
+    const handleSubmit = jest.fn();
+    const handleCheckbox = jest.fn();
     const wrapper = shallow(
       <Note 
         body="You got this!"
+        onSubmit={handleSubmit}
         schedule="daily"
         checkboxLabel="delete"
+        checkboxValue={false}
+        onChange={handleCheckbox}
       />
     );
     expect(wrapper).toMatchSnapshot();
