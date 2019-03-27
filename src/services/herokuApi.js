6ded8a7e660bc9a130aa28ec34e6c8e6963079a9
Kate Dameron <katerj33@gmail.com>
2019-03-27 16:38:09 -0700

    moved deleteRequest service into file with other services

--- src/services/herokuApi.js
+++ src/services/herokuApi.js
@@ -61,3 +61,13 @@ export const getNote = id => {
       return json;
     });
 };
+
+export const deleteRequest = id => {
+  return fetch(`https://note-to-self-server.herokuapp.com/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${getToken(store.getState())}`,
+      'Content-Type': 'application-json',
+    }
+  });
+};
