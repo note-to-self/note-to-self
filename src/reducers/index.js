eb74c325a6f92571df929fa16d634edb3a3ccddd
Kate Dameron <katerj33@gmail.com>
2019-03-27 08:48:11 -0700

    init journal reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import session from './session';
 import message from './message';
+import journal from './journal';
 
 export default combineReducers({
   session,
-  message
+  message,
+  journal
 });
---
+++ src/reducers/journal.js
@@ -0,0 +1,9 @@
+const initialState = {
+  journal: [],
+  favorites: [],
+  filterTerm: ''
+};
+
+export default function reducer(state = initialState, { type, payload }) {
+  return state;
+}
