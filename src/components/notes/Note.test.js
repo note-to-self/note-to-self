ad614b2588ac2efd0e417ed768a67312dd878a49
Kate Dameron <hello@katedameron.com>
2019-03-27 10:50:28 -0700

    Note comp with snapshot (#9)
    * Note comp with snapshot
    
    * updated Note comp with func props and new snap

---
+++ src/components/notes/Note.test.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Note from './Note';
+
+describe('Note snapshot', () => {
+  it('matches a snapshot', () => {
+    const handleSubmit = jest.fn();
+    const handleCheckbox = jest.fn();
+    const wrapper = shallow(
+      <Note 
+        body="You got this!"
+        onSubmit={handleSubmit}
+        schedule="daily"
+        checkboxLabel="delete"
+        checkboxValue={false}
+        onChange={handleCheckbox}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
