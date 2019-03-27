7650105b85ccd86707800e840d6f80694e822f22
Carmen Perezchica <crperezchica@gmail.com>
2019-03-27 13:34:50 -0700

    still working on community (#12)

---
+++ src/components/community/Community.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Search from '../search';
+import Note from '../notes/Note';
+import Notes from '../notes/Notes';
+
+export default function Community() {
+  return (
+    <>
+      <Search />
+      <Note />
+      <Notes />
+    </>
+  );
+}
+
+
+
