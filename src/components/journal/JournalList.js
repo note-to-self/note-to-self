cb8400136c36d0a0bb9ff6f83ed29c0f80d37db9
Kate Dameron <katerj33@gmail.com>
2019-03-27 11:08:37 -0700

    Journal list display component

--- src/components/journal/JournalList.js
+++ src/components/journal/JournalList.js
@@ -1,11 +1,25 @@
-import { connect } from 'react-redux';
-import { getJournalList } from '../../selectors/journal';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Note from '../notes/Note';
 
+export default function JournalList({ journalList }) {
+  const list = journalList.map(note => {
+    const repeat = note.repeat.weekly ? 'weekly' : 'daily';
+    <li>
+      <Note 
+        body={note.body}
+        schedule={repeat}
+        checkboxLabel="Delete Note"
+        checkboxValue={false}
+      />
+    </li>;
+  });
 
-const mapStateToProps = state => ({
-  journalList: getJournalList(state) 
-});
+  return (
+    <ul>{list}</ul>
+  );
+}
 
-export default connect(
-
-)();
+JournalList.propTypes = {
+  journalList: PropTypes.array
+};
