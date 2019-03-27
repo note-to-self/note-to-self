6c6fe6ac41e43970843c3de5e49f4f165da6148a
Kate Dameron <katerj33@gmail.com>
2019-03-27 16:33:52 -0700

    service to get a note by id

--- src/services/herokuApi.js
+++ src/services/herokuApi.js
@@ -46,3 +46,18 @@ export const getUserNotes = id => {
       return json;
     });
 };
+
+export const getNote = id => {
+  return fetch(`${process.env.API_URL}/notes/${id}`, {
+    method: 'GET',
+    headers : {
+      Authorization: `Bearer ${getToken(store.getState())}`,
+      'Content-Type': 'application-json',
+    }
+  })
+    .then(res => [res.ok, res.json()])
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+};
