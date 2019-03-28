b6151e60e4a3a116332b96423846da36e1bbce0f
Carmen Perezchica <crperezchica@gmail.com>
2019-03-28 11:31:09 -0700

    added to search term functionality (#23)

--- src/selectors/search.js
+++ src/selectors/search.js
@@ -1,1 +1,13 @@
+import { getFavorites } from '../selectors/journal';
+// import { getPublicNotes } from '../selectors/community';
+
 export const getSearchTerm = state => state.search.searchTerm;
+
+export const getFileteredNotes = state => {
+  const searchTerm = getSearchTerm(state);
+  return getFavorites(state).filter(note => {
+    const { body } = note;
+    return body.includes(searchTerm);
+  })
+}
+
