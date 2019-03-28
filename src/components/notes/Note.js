ce9332fb8218c0024f0ff1a6ae396995c02104c4
Carmen Perezchica <crperezchica@gmail.com>
2019-03-28 15:22:39 -0700

    border style, font and button styles

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from 'styled-components';
+
+const NotesContainer = styles.form`
+@import url('https://fonts.googleapis.com/css?family=Muli:300,700');
+@media only screen and (max-width: 414px) {
+  background: #D7D6D6;
+  font-family: 'Multi', sans-serif;
+  color: #316f7f;
+  display: block;
+  border: 1pt solid #316f7f;
+  border-radius: 10%;
+  margin-right: auto;
+  margin-left: auto;
+  padding: 0.5em;
+  button {
+    color: #316f7f;
+    display: block;
+    margin: 0.5em;
+    border: 1pt solid #316f7f;
+    border-radius: 10%;
+    padding: 0.5em;
+  }
+}
+`;
 
 export default function Note({ body, handleDelete, schedule, buttonLabel, _id }) {
   return (
-    <section>
-      {handleDelete && <form onSubmit={handleDelete.bind(null, { _id, body })}>
-        <button>{buttonLabel}</button>
-      </form>}
-      <h3>{body}</h3>
-      {schedule && <p>This is a {schedule} affirmation.</p>}
-    </section>
+    <NotesContainer className="container">
+      <section>
+        {handleDelete && <form onSubmit={handleDelete.bind(null, { _id, body })}>
+          <button>{buttonLabel}</button>
+        </form>}
+        <h3>{body}</h3>
+        {schedule && <p>This is a {schedule} affirmation.</p>}
+      </section>
+    </NotesContainer>
   );
 }
 
