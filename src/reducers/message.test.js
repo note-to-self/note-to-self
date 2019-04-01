b7691e2273e657b697246262ec4306470d04a791
Kate Dameron <katerj33@gmail.com>
2019-04-01 15:35:33 -0700

    green tests for UPDATE_MESSAGE and UPDATE_IS_REPEATED actions

--- src/reducers/message.test.js
+++ src/reducers/message.test.js
@@ -1,12 +1,13 @@
 import { 
   CREATE_MESSAGE, 
-  UPDATE_DATE, 
-  // UPDATE_MESSAGE, 
-  // UPDATE_TIME, 
+  UPDATE_DATE,
+  UPDATE_PRIVATE,
+  UPDATE_TIME, 
+  UPDATE_MESSAGE,
+  UPDATE_IS_REPEATED, 
   // UPDATE_DAILY, 
   // UPDATE_IS_REPEATED, 
-  // UPDATE_WEEKLY, 
-  // UPDATE_PRIVATE  
+  // UPDATE_WEEKLY,   
 } from '../actions/message';
 import reducer from './message';
 
@@ -63,16 +64,38 @@ describe('message reducer', () => {
       type: UPDATE_DATE,
       payload: '03/31/2019'
     });
-    expect(updatedState).toEqual({
-      date: '03/31/2019',
-      time: '',
-      privateMessage: false,
-      body: '',
-      repeat: {
-        daily: false,
-        weekly: false
-      },
-      isRepeated: false,
+    expect(updatedState.date).toEqual('03/31/2019');
+  });
+
+  it('can handle UPDATE_PRIVATE action', () => {
+    const updatedState = reducer(state, {
+      type: UPDATE_PRIVATE,
+      payload: true
     });
+    expect(updatedState.privateMessage).toBeTruthy();
+  });
+
+  it('can handle UPDATE_TIME', () => {
+    const updatedState = reducer(state, {
+      type: UPDATE_TIME,
+      payload: '01:00 PM'
+    });
+    expect(updatedState.time).toEqual('01:00 PM');
+  });
+
+  it('can handle UPDATE_MESSAGE action', () => {
+    const updatedState = reducer(state, {
+      type: UPDATE_MESSAGE,
+      payload: 'banana'
+    });
+    expect(updatedState.body).toEqual('banana');
+  });
+
+  it('can handle UPDATE_IS_REPEATED', () => {
+    const updatedState = reducer(state, {
+      type: UPDATE_IS_REPEATED,
+      payload: true
+    });
+    expect(updatedState.isRepeated).toBeTruthy();
   });
 });
