4891df5342edf7b6eb8497f87365c9d75c3d1e15
Kate Dameron <katerj33@gmail.com>
2019-04-01 17:26:44 -0700

    green community reducer test

---
+++ src/reducers/community.test.js
@@ -0,0 +1,21 @@
+import { FETCH_NOTES } from '../actions/community';
+import reducer from './community';
+
+jest.mock('../services/herokuApi');
+
+describe('Quote reducer', () => {
+  let state;
+
+  beforeEach(() => {
+    state = {
+      notes: []
+    };
+  });
+  it('can handle the FETCH_NOTES action', () => {
+    const updatedState = reducer(state, {
+      type: FETCH_NOTES,
+      payload: ['quote', 'banana']
+    });
+    expect(updatedState.notes).toEqual(['quote', 'banana']);
+  });
+});
